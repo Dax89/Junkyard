4552012200f6e487f48ea7d194d5bf0f8406cd0f
Dax <contact@antoniodavide.dev>
2025-03-27 11:25:17 +0100

    feat(C): added CUTL_API define

--- C/cutl.h
+++ C/cutl.h
@@ -14,6 +14,20 @@
 extern "C" {
 #endif
 
+#if defined(_MSC_VER)
+    #if defined(CUTL_EXPORTS)
+        #define CUTL_API __declspec(dllexport)
+    #else
+        #define CUTL_API __declspec(dllimport)
+    #endif
+#else
+    #if defined(CUTL_EXPORTS)
+        #define CUTL_API __attribute__((visibility("default")))
+    #else
+        #define CUTL_API
+    #endif
+#endif
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -57,7 +71,7 @@ static inline void _unreachable_impl(void) { intrinsic_unreachable(); }
 static inline void _unreachable_impl(void) { intrinsic_trap(); }
 #endif
 
-void _except_impl(const char* file, int line, const char* fmt, ...);
+CUTL_API void _except_impl(const char* file, int line, const char* fmt, ...);
 
 #define assume(...)                                                            \
     do {                                                                       \
@@ -99,21 +113,23 @@ typedef struct LinearArena {
     char* end;
 } LinearArena;
 
-LinearArena lineararena_create(isize n);
-LinearArena lineararena_createfrom_n(char* p, isize n);
-void lineararena_reset(LinearArena* self);
-void lineararena_destroy(LinearArena* self);
+CUTL_API LinearArena lineararena_create(isize n);
+CUTL_API LinearArena lineararena_createfrom_n(char* p, isize n);
+CUTL_API void lineararena_reset(LinearArena* self);
+CUTL_API void lineararena_destroy(LinearArena* self);
 
 #define lineararena_createfrom(p) lineararena_createfrom_n(p, sizeof(p))
 
-void* std_allocator(void* ctx, void* ptr, isize osize, isize nsize);
-void* linear_allocator(void* ctx, void* ptr, isize osize, isize nsize);
+CUTL_API void* std_allocator(void* ctx, void* ptr, isize osize, isize nsize);
+CUTL_API void* linear_allocator(void* ctx, void* ptr, isize osize, isize nsize);
 
-void* mem_alloc(isize n, Allocator a, void* ctx);
-void* mem_alloc0(isize n, Allocator a, void* ctx);
-void* mem_realloc(void* ptr, isize osize, isize nsize, Allocator a, void* ctx);
-void* mem_realloc0(void* ptr, isize osize, isize nsize, Allocator a, void* ctx);
-void mem_free(void* ptr, isize n, Allocator a, void* ctx);
+CUTL_API void* mem_alloc(isize n, Allocator a, void* ctx);
+CUTL_API void* mem_alloc0(isize n, Allocator a, void* ctx);
+CUTL_API void* mem_realloc(void* ptr, isize osize, isize nsize, Allocator a,
+                           void* ctx);
+CUTL_API void* mem_realloc0(void* ptr, isize osize, isize nsize, Allocator a,
+                            void* ctx);
+CUTL_API void mem_free(void* ptr, isize n, Allocator a, void* ctx);
 
 #define mem_alloctype_n(T, n, a, ctx) (T*)mem_alloc(sizeof(T) * n, a, ctx)
 #define mem_alloctype0_n(T, n, a, ctx) (T*)mem_alloc0(sizeof(T) * n, a, ctx)
@@ -154,10 +170,10 @@ void mem_free(void* ptr, isize n, Allocator a, void* ctx);
         (self)->data[(self)->length++] = __VA_ARGS__;                          \
     } while(0)
 
-void slice_init(void* self, Allocator a, void* ctx);
-void _slice_reserve(void* self, isize n, isize dsize);
-void _slice_grow(void* self, isize dsize);
-void _slice_destroy(void* self, isize dsize);
+CUTL_API void slice_init(void* self, Allocator a, void* ctx);
+CUTL_API void _slice_reserve(void* self, isize n, isize dsize);
+CUTL_API void _slice_grow(void* self, isize dsize);
+CUTL_API void _slice_destroy(void* self, isize dsize);
 // *** Slice *** //
 
 // *** String *** //
@@ -175,28 +191,28 @@ typedef struct StrSplit {
 #define str_ismutable(self) ((self)->alloc != NULL)
 #define str_empty slice_empty
 
-Str str_create_n(const char* s, isize n, Allocator a, void* ctx);
-Str str_view_n(const char* s, isize n);
-Str str_sub(const Str* self, isize start, isize end);
-Str str_dup(const Str* self);
-Str* str_dup_to(const Str* self, Str* dest);
-void str_clear(Str* self);
-void str_ncat(Str* self, const char* s, isize n);
-void str_reserve(Str* self, isize n);
-void str_destroy(Str* self);
-void str_deleterange(Str* self, isize start, isize end);
-void str_tolower(Str* self);
-void str_toupper(Str* self);
-void str_trim(Str* self);
-usize str_hash(const Str* self);
-bool str_equals(const Str* self, const Str* rhs);
-bool _str_startswith_n(const Str* self, const char* s, isize n);
-bool _str_endswith_n(const Str* self, const char* s, isize n);
-isize _str_indexof_n(const Str* self, const char* s, isize n);
-isize _str_lastindexof_n(const Str* self, const char* s, isize n);
-void _str_insert_n(Str* self, isize idx, const char* s, isize n);
-void _str_replace_n(Str* self, const char* from, isize nfrom, const char* to,
-                    isize nto);
+CUTL_API Str str_create_n(const char* s, isize n, Allocator a, void* ctx);
+CUTL_API Str str_view_n(const char* s, isize n);
+CUTL_API Str str_sub(const Str* self, isize start, isize end);
+CUTL_API Str str_dup(const Str* self);
+CUTL_API Str* str_dup_to(const Str* self, Str* dest);
+CUTL_API void str_clear(Str* self);
+CUTL_API void str_ncat(Str* self, const char* s, isize n);
+CUTL_API void str_reserve(Str* self, isize n);
+CUTL_API void str_destroy(Str* self);
+CUTL_API void str_deleterange(Str* self, isize start, isize end);
+CUTL_API void str_tolower(Str* self);
+CUTL_API void str_toupper(Str* self);
+CUTL_API void str_trim(Str* self);
+CUTL_API usize str_hash(const Str* self);
+CUTL_API bool str_equals(const Str* self, const Str* rhs);
+CUTL_API bool _str_startswith_n(const Str* self, const char* s, isize n);
+CUTL_API bool _str_endswith_n(const Str* self, const char* s, isize n);
+CUTL_API isize _str_indexof_n(const Str* self, const char* s, isize n);
+CUTL_API isize _str_lastindexof_n(const Str* self, const char* s, isize n);
+CUTL_API void _str_insert_n(Str* self, isize idx, const char* s, isize n);
+CUTL_API void _str_replace_n(Str* self, const char* from, isize nfrom,
+                             const char* to, isize nto);
 
 static inline bool str_startswith(const Str* self, const char* s) {
     return _str_startswith_n(self, s, strlen(s));
@@ -268,8 +284,8 @@ static inline void str_replace_str(Str* self, const Str* from, const Str* to) {
     _str_replace_n(self, from->data, from->length, to->data, to->length);
 }
 
-StrSplit _strsplit_create_n(const Str* self, const char* sep, isize n);
-Str str_split_next(StrSplit* it);
+CUTL_API StrSplit _strsplit_create_n(const Str* self, const char* sep, isize n);
+CUTL_API Str str_split_next(StrSplit* it);
 
 static inline StrSplit strsplit_create(Str* self, const char* sep) {
     return _strsplit_create_n(self, sep, strlen(sep));
@@ -316,10 +332,10 @@ typedef struct List {
 #define list_foreach(it, self, T, m)                                           \
     for(it = list_first((self), T, m); !!(it); it = list_next(it, T, m))
 
-void list_init(List* self);
-void list_push_tail(List* self, ListNode* n);
-void list_push(List* self, ListNode* n);
-void list_del(List* self, ListNode* n);
+CUTL_API void list_init(List* self);
+CUTL_API void list_push_tail(List* self, ListNode* n);
+CUTL_API void list_push(List* self, ListNode* n);
+CUTL_API void list_del(List* self, ListNode* n);
 // *** List *** //
 
 // *** HList *** //
@@ -338,8 +354,8 @@ typedef struct HList {
     for(it = (self)->first ? hlist_item((self)->first, T, m) : NULL; !!(it);   \
         it = it->m.next ? hlist_item(it->m.next, T, m) : NULL)
 
-void hlist_push(HList* self, HListNode* n);
-void hlist_del(HListNode* n);
+CUTL_API void hlist_push(HList* self, HListNode* n);
+CUTL_API void hlist_del(HListNode* n);
 // *** HList *** //
 
 // *** Map *** //
@@ -385,9 +401,9 @@ typedef uptr (*HMapHash)(const void*);
         }                                                                      \
     }
 
-void _hmap_init(HList* self, usize n);
-uptr _hmap_directhash(const void* p);
-unsigned long _hmap_bits(unsigned long cap);
+CUTL_API void _hmap_init(HList* self, usize n);
+CUTL_API uptr _hmap_directhash(const void* p);
+CUTL_API unsigned long _hmap_bits(unsigned long cap);
 // *** Map *** //
 
 #if defined(__cplusplus)
